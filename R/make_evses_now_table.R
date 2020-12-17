22f5f435672296b14fd5a625f5f3e99abf7b1842
Chintan Pathak <chintan.pathak@gmail.com>
2020-12-16 23:05:46 -0800

    commented out make_evses_table.R

--- R/make_evses_now_table.R
+++ R/make_evses_now_table.R
@@ -1,36 +1,36 @@
-
-if (!DBI::dbCanConnect(
-  RPostgres::Postgres(),
-  host = Sys.getenv("MAIN_HOST"),
-  dbname = Sys.getenv("MAIN_DB"),
-  user = Sys.getenv("MAIN_USER"),
-  password = Sys.getenv("MAIN_PWD"),
-  port = Sys.getenv("MAIN_PORT")
-)) {
-  lg$log(level = "fatal",
-         msg = "Cannot connect to database",
-         "ip" = ipify::get_ip())
-  # Exit if DB cannot connect
-  stop("Cannot connect to database")
-}
-
-main_con <- DBI::dbConnect(
-  RPostgres::Postgres(),
-  host = Sys.getenv("MAIN_HOST"),
-  dbname = Sys.getenv("MAIN_DB"),
-  user = Sys.getenv("MAIN_USER"),
-  password = Sys.getenv("MAIN_PWD"),
-  port = Sys.getenv("MAIN_PORT")
-)
-
-
-aids <- DBI::dbGetQuery(main_con, 'select analysis_id from analysis_record where analysis_id NOT IN (select distinct analysis_id from evses_now)')$analysis_id
-
-print(aids)
-
-for (a_id in aids) {
-  tripgen::make_evses_now_table(main_con, a_id)
-}
-
-
-
+#
+# if (!DBI::dbCanConnect(
+#   RPostgres::Postgres(),
+#   host = Sys.getenv("MAIN_HOST"),
+#   dbname = Sys.getenv("MAIN_DB"),
+#   user = Sys.getenv("MAIN_USER"),
+#   password = Sys.getenv("MAIN_PWD"),
+#   port = Sys.getenv("MAIN_PORT")
+# )) {
+#   lg$log(level = "fatal",
+#          msg = "Cannot connect to database",
+#          "ip" = ipify::get_ip())
+#   # Exit if DB cannot connect
+#   stop("Cannot connect to database")
+# }
+#
+# main_con <- DBI::dbConnect(
+#   RPostgres::Postgres(),
+#   host = Sys.getenv("MAIN_HOST"),
+#   dbname = Sys.getenv("MAIN_DB"),
+#   user = Sys.getenv("MAIN_USER"),
+#   password = Sys.getenv("MAIN_PWD"),
+#   port = Sys.getenv("MAIN_PORT")
+# )
+#
+#
+# aids <- DBI::dbGetQuery(main_con, 'select analysis_id from analysis_record where analysis_id NOT IN (select distinct analysis_id from evses_now)')$analysis_id
+#
+# print(aids)
+#
+# for (a_id in aids) {
+#   tripgen::make_evses_now_table(main_con, a_id)
+# }
+#
+#
+#
